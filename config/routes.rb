8cac2b9ea91548326e87e2da50ba2107d1f5b57e
Tester <test>
2014-05-01 13:09:03 -0500

    added home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Weekexam::Application.routes.draw do
+  resources :homes
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
